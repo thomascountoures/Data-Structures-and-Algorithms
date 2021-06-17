6db5b02a34c247ff773f95cce2fabe2560775ffc
Thomas Countoures <rainwave@gmail.com>
2021-06-16 23:22:49 -0700

    add bst remove comments

--- trees.js
+++ trees.js
@@ -40,23 +40,51 @@ class BinarySearchTree {
       if (value < currentNode) this.remove(value, currentNode.left, currentNode);
       else if (value > currentNode) this.remove(value, currentNode.right, currentNode);
       else {
+        // Remember: 'value' === currentNode.value (currentNode is the node with the matching value)
+
+        // If the matching node has no children (easy mode).
         if (currentNode.left === null && currentNode.right === null) {
+          // If this node is the root, set root to null.
           if (currentNode === this.root) this.root = null;
           else if (value < parentNode.value) parentNode.left = null;
           else parentNode.right = null;
+
+          // If the matching node has one child, on the left side.
         } else if (currentNode.left && currentNode.right === null) {
           if (currentNode === this.root) this.root = currentNode.left;
           else if (currentNode.left < parentNode.value) parentNode.left = currentNode.left;
           else parentNode.right = currentNode.left;
+
+          // If the matching node has one child, on the right side.
         } else if (currentNode.left === null && currentNode.right) {
           if (currentNode === this.root) this.root = currentNode.right;
           else if (currentNode.right < parentNode.value) parentNode.left = currentNode.right;
           else parentNode.right = currentNode.right;
+
+          // If the matching node has two children (hard mode).
         } else {
+          // Get the leftmost child of the current node's right child.
+          // Ie. move one node to the right, and then go all the way to the bottom left.
           const leftMostOfRightChild = this.getLeftMost(currentNode.right);
+
+          // Attach the current node's left child to the bottom of that left most child
+          // retrieved above. At this point, we have created chain of nodes - a combination
+          // of the left most node of the current node's right child, and all of the left nodes
+          // of the current node underneath that.
+          //
+          // This sounds a bit complicated, but basically we've just
+          // created a long chain of nodes that still follow the rules of a BST.
           leftMostOfRightChild.left = currentNode.left;
-          if (currentNode.value < parentNode.value) parentNode.left = currentNode.right;
-          else if (currentNode.value > parentNode.value) parentNode.right = currentNode.right;
+
+          // If the matching node's value is less than the parent node's value,
+          // attach the recently created string of nodes on the current node's
+          // right side on the left side of the parent node.
+          if (value < parentNode.value) parentNode.left = currentNode.right;
+          // And vice versa.
+          else if (value > parentNode.value) parentNode.right = currentNode.right;
+          // If the matching node's value is not greater or less than the parent node's
+          // value, that means this is the root, since a BST cannot have duplicate values.
+          // We update the current root to our newly constructed set of nodes.
           else this.root = currentNode.right;
         }
       }
